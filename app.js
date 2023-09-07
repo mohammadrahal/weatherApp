8c593ee03718ebcc13a77978011944dba2e1bcce
Mohammad Rahal <rahalmohamad21@gmail.com>
2023-09-07 13:59:47 +0300

    fixing css

--- app.js
+++ app.js
@@ -1,72 +1,59 @@
 console.log("hello world!")
 
 const cityname = document.getElementById('city-input');
-const searchbtn = document.getElementById('search')
-const cityName = document.getElementById('city-name')
-const time1 = document.getElementById('time1');
-const time2 = document.getElementById('time2');
-const time3 = document.getElementById('time3');
-const time4 = document.getElementById('time4');
-const day1 = document.getElementById('day1');
-const day2 = document.getElementById('day2');
-const day3 = document.getElementById('day3');
-const day4 = document.getElementById('day4');
-const temperature1 = document.getElementById('temperature1');
-const temperature2 = document.getElementById('temperature2');
-const temperature3 = document.getElementById('temperature3');
-const temperature4 = document.getElementById('temperature4');
+const searchbtn = document.getElementById('search');
+const cityName = document.getElementById('city-name');
+const iconElement = document.querySelector(".icon");
+
+// get from html
+const temperatureElements = [temperature1, temperature2, temperature3, temperature4];
+const timeElements = [time1, time2, time3, time4];
+const dayElements = [day1, day2, day3, day4];
 
 
+const getData = async () => {
+    try {
+        const city = cityname.value.trim();
 
+        if (city) {
+            const apiKey = '61af75f3f18c7bf8d2b68a365c65c688';
+            const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
-// method 1
-// searching for city 
-searchbtn.addEventListener('click', function () {
-    console.log('hello');
-    const city = cityname.value.trim();
+            const response = await fetch(apiUrl);
+            if (!response.ok) {
+                throw new Error('Network error');
+            }
 
-    if (city) {
-        const apiKey = '61af75f3f18c7bf8d2b68a365c65c688';
-        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+            const data = await response.json();
+            cityName.textContent = data.name;
 
-        const getData = async () => {
-            try {
-                const response = await fetch(apiUrl);
-                if (!response.ok) {
-                    throw new Error('Network error');
-                }
-
-                const data = await response.json();
-                cityName.textContent = data.name;
-                temperature1.textContent = data.main.temp;
-                temperature2.textContent = data.main.temp;
-                temperature3.textContent = data.main.temp;
-                temperature4.textContent = data.main.temp;
-
+            for (let i = 0; i < temperatureElements.length; i++) {
+                temperatureElements[i].textContent = data.main.temp;
                 const sunriseTimestamp = data.sys.sunrise;
                 const sunriseDate = new Date(sunriseTimestamp * 1000);
                 const hours = sunriseDate.getHours();
                 const day = sunriseDate.getDate();
-
-                time1.textContent = ` ${hours}`;
-                time2.textContent = ` ${hours}`;
-                time3.textContent = ` ${hours}`;
-                time4.textContent = ` ${hours}`;
-
-                day1.textContent = ` ${day}`;
-                day2.textContent = ` ${day}`;
-                day3.textContent = ` ${day}`;
-                day4.textContent = ` ${day}`;
-            } catch (error) {
-                console.error('There was a problem with the fetch operation:', error);
+                timeElements[i].textContent = `${hours}`;
+                dayElements[i].textContent = `${day}`;
             }
-        };
+            const weatherIconCode = data.weather[0].icon;
 
-        getData();
-    } else {
-        alert('Please enter a city name.');
+            const iconElement = document.querySelector(".icon img");
+            iconElement.src = `https://openweathermap.org/img/wn/${weatherIconCode}.png`;
+
+            // Set the src attribute of the iconElement to the weather icon URL
+            // iconElement.src = weatherIconUrl;
+        } else {
+            alert('Please enter a city name.');
+        }
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
     }
-});
+};
+
+searchbtn.addEventListener('click', getData);
+
+
 
 // method 2
 // fetch(api)
@@ -91,5 +78,3 @@ searchbtn.addEventListener('click', function () {
 //     .catch(error => {
 //         console.error('There was a problem with the fetch operation:', error);
 //     });
-
-//method 2 
